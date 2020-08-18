0676e9685691f3b746fdd0b4888fc5ff97385c0b
Dmitry Grizlov <63951854+Dmitry-Grizlov@users.noreply.github.com>
2020-08-18 14:32:19 +0300

    Add files via upload

--- Calculator.cpp
+++ Calculator.cpp
@@ -9,60 +9,72 @@ double a; //first number
 double b; // second number
 string c; // action
 double result; //result
+char x; // exit
 
 
 int
 main ()
 {
-    cout <<
-    "Hi!Choose one of the actions: sum/min/div/mult" << endl;
-    cin >> c;
-    if (c == "sum"){
+    
+    do
+    {
+    
+        cout <<
+        "Hi!Choose one of the actions: sum/min/div/mult" << endl;
+        cin >> c;
+        if (c == "sum"){
+            
+            cout << "Enter your first number:" << endl;
+            cin >> a;
+            cout << "Enter the second one:" << endl;
+            cin >> b;
+            result = a + b;
+            cout << "Your result is: " << result << endl;    
         
-        cout << "Enter your first number:" << endl;
-        cin >> a;
-        cout << "Enter the second one:" << endl;
-        cin >> b;
-        result = a + b;
-        cout << "Your result is: " << result << endl;    
+        }
+        
+        if (c == "min")
+        {
     
+            cout << "Enter your first number:" << endl;
+            cin >> a;
+            cout << "Enter the second one:" << endl;
+            cin >> b;
+            result = a - b;
+            cout << "Your result is: " << result << endl;
+    
+        }
+    
+        if (c == "div")
+        {
+    
+            cout << "Enter your first number:" << endl;
+            cin >> a;
+            cout << "Enter the second one:" << endl;
+            cin >> b;
+            result = a / b;
+            cout << "Your result is: " << result << endl;
+    
+        }
+    
+        if (c == "mult")
+        {
+    
+            cout << "Enter your first number:" << endl;
+            cin >> a;
+            cout << "Enter the second one:" << endl;
+            cin >> b;
+            result = a * b;
+            cout << "Your result is: " << result << endl;
+    
+        }
+        
+        cout << "Enter 'X' to exit" <<endl;
+        cin >> x;
+        
     }
     
-    if (c == "min")
-    {
-
-        cout << "Enter your first number:" << endl;
-        cin >> a;
-        cout << "Enter the second one:" << endl;
-        cin >> b;
-        result = a - b;
-        cout << "Your result is: " << result << endl;
-
-    }
-
-    if (c == "div")
-    {
-
-        cout << "Enter your first number:" << endl;
-        cin >> a;
-        cout << "Enter the second one:" << endl;
-        cin >> b;
-        result = a / b;
-        cout << "Your result is: " << result << endl;
-
-    }
-
-    if (c == "mult")
-    {
-
-        cout << "Enter your first number:" << endl;
-        cin >> a;
-        cout << "Enter the second one:" << endl;
-        cin >> b;
-        result = a * b;
-        cout << "Your result is: " << result << endl;
-
-    }
+    while (x != 'X');
 
     return 0;
 }
